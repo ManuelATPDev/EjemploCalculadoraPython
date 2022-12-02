6cdaf9052da097472b2e5af005024b57a89a2fc7
Manuel Torrealba <78187170+ManuelATPDev@users.noreply.github.com>
2022-12-02 22:08:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Crear una calculadora con las operaciones de suma, resta, multiplicación y división
+
+valor1 = 7;
+valor2 = 4;
+
+resultadoSuma = valor1 + valor2;
+resultadoResta = valor1 - valor2;
+resultadoMultiplicacion = valor1 * valor2;
+resultadoDivision = valor1 / valor2;
+
+print("La suma del número ",valor1," y el numero ",valor2," es: ",resultadoSuma);
+print("La resta del número ",valor1," y el numero ",valor2," es: ",resultadoResta);
+print("La multiplicación del número ",valor1," y el numero ",valor2," es: ",resultadoMultiplicacion);
+print("La división del número ",valor1," y el numero ",valor2," es: ",resultadoDivision);
